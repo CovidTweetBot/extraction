405ed9fe96104706705b45dc1e40e5fe21a02239
Reiner Marquez <rmaceissoft@gmail.com>
2021-06-08 16:41:17 -0500

    persist history of content length values into a text file

--- vacunas2.py
+++ vacunas2.py
@@ -1,17 +1,25 @@
+from datetime import datetime
 from urllib.request import Request, urlopen
 
-# Requests
-csv_url = "https://cloud.minsa.gob.pe/s/ZgXoXqK2KLjRLxD/download"
+# url to the dataset (CSV format) containing the nominal vaccination records
+CSV_URL = "https://cloud.minsa.gob.pe/s/ZgXoXqK2KLjRLxD/download"
+
+CHANGE_INDICATOR_FILENAME = "./data/change_indicator.txt"
 
 
 def main():
-    print('Reading database from link')
-    req = Request(csv_url, headers={'User-Agent': 'Mozilla/5.0'})
+    print(f"Reading database from link: {CSV_URL}")
+    req = Request(CSV_URL, headers={'User-Agent': 'Mozilla/5.0'})
 
     rsp = urlopen(req)
 
     content_length = dict(rsp.getheaders())['Content-Length']
-    print(f"Content-Length={content_length}")
+
+    msg = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}; Content-Length={content_length}"
+    with open(CHANGE_INDICATOR_FILENAME, "a+") as f:
+        f.write(f"{msg}\n")
+
+    print(msg)
 
 
 if __name__ == "__main__":
